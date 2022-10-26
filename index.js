ec365efaad43d4a27b0855584c16755984655b4f
Jules Joseph <julesj93@gmail.com>
2022-10-26 01:27:13 -0400

    Added consts to index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const Employee = require('./Employee.test.js');
+const Engineer = require('./Engineer.test.js');
+const Manager = require('./Manager.test.js');
+const Intern =require('./Intern.test.js');
