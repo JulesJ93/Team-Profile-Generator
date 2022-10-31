5e38095cd5700275601b9a3855e93b02eb12481f
Jules Joseph <julesj93@gmail.com>
2022-10-31 00:20:25 -0400

    added pkg,json in terminal

--- index.js
+++
@@ -1,4 +0,0 @@
-const Employee = require('./Employee.test.js');
-const Engineer = require('./Engineer.test.js');
-const Manager = require('./Manager.test.js');
-const Intern =require('./Intern.test.js');
