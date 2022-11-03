fa8c4b0a3608c490fc6b554f416d1e81bce9044b
Jules Joseph <julesj93@gmail.com>
2022-11-03 01:39:24 -0400

    rearranged index.js to have correct prompts to accomodate user input

--- index.js
+++ index.js
@@ -5,26 +5,46 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 
+
 inquirer
   .prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is the Team manager name?',
-    },
-    {
-      type: 'password',
-      message: 'What is your password?',
-      name: 'password',
-    },
-    {
-      type: 'password',
-      message: 'Re-enter password to confirm:',
-      name: 'confirm',
-    },
-  ])
-  .then((answers) =>
-    answers.confirm === answers.password
-      ? console.log(`Welcome to the club...${answers.username}`)
-      : console.log('You forgot your password already?!')
-  );
+      validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log ("Please enter the manager's name!");
+            return false; 
+        }
+    }
+},
+{
+  type: 'input',
+  name: 'id',
+  message: "Please enter the employee's ID.",
+  validate: nameInput => {
+      if  (isNaN(nameInput)) {
+          console.log ("Please enter the employee's ID!")
+          return false; 
+      } else {
+          return true;
+      }
+  }
+},
+{
+  type: 'input',
+  name: 'email',
+  message: "Please enter the employee's email.",
+  validate: email => {
+      valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+      if (valid) {
+          return true;
+      } else {
+          console.log ('Please enter an email!')
+          return false; 
+      }
+  }
+},
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,12 +1,3 @@
 const Employee = require('.lib\Employee.js')
 
 
-
-
-const name= this.name
-const id= this.id
-const email = this.email
-const getName()= this.getName
-const getID()= this.getID
-const getEmail()= this.email
-const getRole()= this.getrole.return.employee
