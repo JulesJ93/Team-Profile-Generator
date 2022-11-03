336f0903def25d427ce4d602a4123499746b46c1
Jules Joseph <julesj93@gmail.com>
2022-11-03 00:47:56 -0400

    added structure to run inquierer

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const inquirer = require('inquirer');
+
+nquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((answers) =>
+    answers.confirm === answers.password
+      ? console.log(`Welcome to the club...${answers.username}`)
+      : console.log('You forgot your password already?!')
+  );
