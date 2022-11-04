0b4ee4d9c218f30b72391807b33989629e510b6e
Jules Joseph <julesj93@gmail.com>
2022-11-03 23:24:21 -0400

    added comments for the engineer test method

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -2,23 +2,23 @@ const Engineer = require("../lib/Engineer");
 
 describe("create a new engineer", () => {
     const myEngineer = new Engineer("Jules", 5209,"Julesj93@gmail.com","Julesj93");
-
+//checks for engineer name
     test("check name",() => {
         expect(myEngineer.name).toEqual("Jules");
     });
-
+//checks for the engineer id
     test("check Id", () => {
         expect(myEngineer.getId()).toEqual(5209);
     });
-        
+//checks for the engineers email        
       test("check engineers Email", () => {
         expect(myEngineer.getEmail()).toEqual("Jules93@gmail.com");
     });
-       
+//checks for the engineers github       
     test("check GitHub", () => {
     expect(myEngineer.getGitHub()).toEqual("Julesj93");
     });
-
+//checks for the engineers role
     test("check Role", () => {
     expect(myEngineer.getRole()).toEqual("Engineer");
     });
