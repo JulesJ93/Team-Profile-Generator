b34bb8d1bf4a34f56a5387cbe3f8ebf946ab76c6
Jules Joseph <julesj93@gmail.com>
2022-11-03 23:14:09 -0400

    added test templates for engineer prompt test

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -2,3 +2,16 @@ const Engineer = require("../lib/Engineer");
 
 describe)"create a new engineer", () =>
     const myEngineer = new Engineer("Jules", 5209,"jules@mymail.com","Julesj93");
+
+    test("check name",() => {
+        expect(myEngineer.name).toEqual("Jules");
+    });
+    test("check name",() => {
+        expect(myEngineer.name).toEqual("Jules");
+    });test("check name",() => {
+        expect(myEngineer.name).toEqual("Jules");
+    });test("check name",() => {
+        expect(myEngineer.name).toEqual("Jules");
+    });test("check name",() => {
+        expect(myEngineer.name).toEqual("Jules");
+    });
