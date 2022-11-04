65dff8af21c03c29146955ad4c640f48c5a88551
Jules Joseph <julesj93@gmail.com>
2022-11-03 23:03:25 -0400

    reformatting engineer test

--- tests/Engineer.test.js
+++
@@ -1,9 +0,0 @@
-class Engineer {}
-
-const Employee = require('./Employee.test.js');
-
-class Engineer extends Employee {
-
-    const github= this.julesj93
-    const getgithub()=getgithub
-    const getRole()=//Overridden to return 'intern'
