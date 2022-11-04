0003a5706305220d97b990e384140d7fb72bd3d7
Jules Joseph <julesj93@gmail.com>
2022-11-03 23:17:09 -0400

    added test to check for engineer email

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,17 +1,21 @@
 const Engineer = require("../lib/Engineer");
 
 describe)"create a new engineer", () =>
-    const myEngineer = new Engineer("Jules", 5209,"jules@mymail.com","Julesj93");
+    const myEngineer = new Engineer("Jules", 5209,"Jules@mymail.com","Julesj93");
 
     test("check name",() => {
         expect(myEngineer.name).toEqual("Jules");
     });
+
     test("check Id", () => {
         expect(myEngineer.getId()).toEqual(5209);
       });
-        expect(myEngineer.name).toEqual("Jules");
-    });test("check name",() => {
-        expect(myEngineer.name).toEqual("Jules");
+        
+      test("check engineers Email", () => {
+    expect(myEngineer.getEmail()).toEqual("Jules@mymail.com");
+  });
+       
+  expect(myEngineer.name).toEqual("Jules");
     });test("check name",() => {
         expect(myEngineer.name).toEqual("Jules");
     });
