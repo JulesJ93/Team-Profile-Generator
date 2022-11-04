711e39f5f798c95295ef6ce0fd5e6873c36f817f
Jules Joseph <julesj93@gmail.com>
2022-11-03 23:42:19 -0400

    added const to intern also correct intern source file had a , instead of .

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,8 +1,25 @@
 const Manager = require ('..lib\Manager.js')
 
-const Employee = require('./Employee.test.js');
-
-class Manager extends Employee {
-
-    const officeNumber= this.officeNumber
-    const getRole=//Overridden to return 'Manager'
+describe("create a manager", () => {
+    const myManager = new Manager("Jules", 5209, "Julesj93@gmail.com", 1);
+  //test to check name
+    test("check name", () => {
+      expect(myManager.getName()).toEqual("Jules");
+    });
+ //test to check id 
+    test("check Id", () => {
+      expect(myManager.getId()).toEqual(5209);
+    });
+  //test to check email
+    test("check Email", () => {
+      expect(myManager.getEmail()).toEqual("Julesj93@gmail.com");
+    });
+  //test to check office number
+    test("check OfficeNumber", () => {
+      expect(myManager.getOfficeNumber()).toEqual(1);
+    });
+  //test to check role
+    test("check Role", () => {
+      expect(myManager.getRole()).toEqual("Manager");
+    });
+  });
